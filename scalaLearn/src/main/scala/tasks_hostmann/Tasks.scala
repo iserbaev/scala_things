aeb6fad5f7bec7234941305ade4c16d150f2a406
ilnur serbaev <serbaevilnur@gmail.com>
2016-11-16 14:00:02 +0500

    add tasks

--- scalaLearn/src/main/scala/tasks_hostmann/Tasks.scala
+++ scalaLearn/src/main/scala/tasks_hostmann/Tasks.scala
@@ -16,4 +16,13 @@ object Tasks extends App{
   println(codesSumOf1("Hello"))
   println(codesSumOf2("Hello"))
   println(codesSumOf3("Hello"))
+
+  def pow_ch2_tsk10(x:Int, i:Int):Int = {
+    if (i==0) 1
+    else if (i%2==0 && i>0) pow_ch2_tsk10(x, i/2)*pow_ch2_tsk10(x, i/2)
+    else if (i%2 !=0 && i>0) x*pow_ch2_tsk10(x, i-1)
+    else 1/pow_ch2_tsk10(x, -i)
+  }
+
+  println(pow_ch2_tsk10(2,2))
 }
