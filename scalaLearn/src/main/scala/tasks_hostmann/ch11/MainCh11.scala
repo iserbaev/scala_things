8f38814a54ff482e79aa8f9164aa2121f0a1e960
ilnur serbaev <serbaevilnur@gmail.com>
2016-11-30 15:38:23 +0500

    add tasks

--- scalaLearn/src/main/scala/tasks_hostmann/ch11/MainCh11.scala
+++ scalaLearn/src/main/scala/tasks_hostmann/ch11/MainCh11.scala
@@ -4,5 +4,10 @@ package tasks_hostmann.ch11
   * Created by ilnur on 30.11.16.
   */
 object MainCh11 extends App{
-
+  /**
+    * ch11_tsk4
+    */
+  println(Money(1,75)+Money(0,50)==Money(2,25))
+  println(Money(1,75)+Money(0,50)<Money(2,26))
+  println(Money(1,75)+Money(0,50)>Money(2,26))
 }
