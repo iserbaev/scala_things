b64f841e71ce25818b0d93d052bc8a2614da29cf
ilnur serbaev <serbaevilnur@gmail.com>
2016-11-30 13:35:05 +0500

    add tasks

---
+++ scalaLearn/src/main/scala/tasks_hostmann/ch11/MainCh11.scala
@@ -0,0 +1,8 @@
+package tasks_hostmann.ch11
+
+/**
+  * Created by ilnur on 30.11.16.
+  */
+object MainCh11 extends App{
+
+}
---
+++ scalaLearn/src/main/scala/tasks_hostmann/ch11/Money.scala
@@ -0,0 +1,9 @@
+package tasks_hostmann.ch11
+
+/**
+  * Created by ilnur on 30.11.16.
+  * ch11_tsk4
+  */
+class Money(dollar:Int, cents:Int) {
+
+}
