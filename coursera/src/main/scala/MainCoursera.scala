2a21063f95a75186dbfd91018ce99eb9393367fd
ilnur <serbaevilnur@gmail.com>
2016-12-08 16:13:04 +0500

    add tasks

--- coursera/src/main/scala/MainCoursera.scala
+++ coursera/src/main/scala/MainCoursera.scala
@@ -52,30 +52,45 @@ object MainCoursera extends App{
     * wk2_tsk4
     */
   class Rational(x:Int, y:Int){
+    require(y!=0,"denominator must be non zero")
     private def gcd(a:Int, b:Int):Int = if(b==0)a else gcd(b,a%b)
     private val g = gcd(x,y)
     def numer = x / g
     def denom = y/ g
 
-    def add(that: Rational) =
+    def + (that: Rational) =
       new Rational(
         numer*that.denom+that.numer*denom,
         denom*that.denom
       )
 
-    def neg = new Rational(-numer, denom)
-    def sub(that:Rational) = add(that.neg)
-    def div(that:Rational) =
+    def unary_- : Rational = new Rational(-numer, denom)
+    def unary_+ : Rational = new Rational(numer, denom)
+    def - (that:Rational): Rational = this + -that
+    def / (that:Rational) =
       new Rational(
         numer*that.denom,
         denom*that.numer
       )
-    def mul(that:Rational) =
+    def * (that:Rational) =
       new Rational(
         numer*that.numer,
         denom*that.denom
       )
 
+    def < (that:Rational) = numer*that.denom < that.numer*denom
+    def max (that:Rational) = if (this < that) that else this
     override def toString() = numer + "/" + denom
   }
+  val x = new Rational(1,3)
+  val y = new Rational(5,7)
+  val z = new Rational(3,2)
+  x + y
+  -x
+  +x
+  x - y
+  x - y
+  x - y - z
+  x + y * z
+  x < y
 }
