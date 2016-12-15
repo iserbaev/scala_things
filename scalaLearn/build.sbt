ed7bbbbcb19f7f8eba2c34e68ac85b5c4017e979
ilnur serbaev <serbaevilnur@gmail.com>
2016-12-15 13:03:39 +0500

    add tasks

--- scalaLearn/build.sbt
+++ scalaLearn/build.sbt
@@ -8,6 +8,17 @@ val akkaVersion = "2.4.12"
 
 val kamonVersion = "0.6.3"
 
+// https://mvnrepository.com/artifact/javax.persistence/persistence-api
+libraryDependencies += "javax.persistence" % "persistence-api" % "1.0"
+
+// https://mvnrepository.com/artifact/org.scala-lang/scala-reflect
+libraryDependencies += "org.scala-lang" % "scala-reflect" % "2.12.0"
+
+// https://mvnrepository.com/artifact/javax.inject/javax.inject
+libraryDependencies += "javax.inject" % "javax.inject" % "1"
+
+
+
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % akkaVersion,
   "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
---
+++ scalaLearn/src/main/scala/tasks_hostmann/ch14/Sample.scala
@@ -0,0 +1,26 @@
+package tasks_hostmann.ch14
+
+import java.io.IOException
+
+import scala.beans.BeanProperty
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.inject.{Inject, Named}
+
+import com.sun.istack.internal.NotNull
+
+import scala.annotation.{tailrec, varargs}
+
+/**
+  * Created by ilnur on 15.12.16.
+  */
+@deprecated
+@throws[java.rmi.RemoteException] @SerialVersionUID(656465464654321654L)
+@Entity class Sample [@specialized T] @Inject() (@Id @BeanProperty @NotNull id:String, @BeanProperty name: String, title:T) extends Cloneable{
+  @BeanProperty @Named("pass") val password:String = _
+  @volatile var x:Integer = _
+  @transient var none:String = _
+  @throws(classOf[IOException]) def readSample(path: String):String = ""
+  @varargs def setProperties(props:String*):Unit = {}
+  @tailrec final def sum(xs: Seq[Int], partial: Int): Int = if (xs.isEmpty) partial else sum(xs.tail, xs.head+partial)
+}
