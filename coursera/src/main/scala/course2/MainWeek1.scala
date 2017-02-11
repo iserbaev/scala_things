aa932def3471a4e53b183f6e31d3e0d276c71b07
ilnur <ilnur.serbaev@dz.ru>
2017-02-11 15:25:58 +0500

    week 1, lesson 1.2

--- coursera/src/main/scala/course2/MainWeek1.scala
+++ coursera/src/main/scala/course2/MainWeek1.scala
@@ -1,12 +1,13 @@
 package course2
 
 /**
-  * Created by ilnur on 11.02.17.
+  * week 1 - for expressions and monads
   */
 object MainWeek1 extends App{
   /**
-    * lecture 1.1 - Queries
+    * lecture 1.1 - Queries with for
     */
+  case class Book (title: String, authors: List[String])
   val books: List[Book] = List(
     Book(title = "Structure and Interpretation of Computer Programs",
       authors = List("Abelson, Harald", "Sussman, Gerald J.")),
@@ -21,7 +22,6 @@ object MainWeek1 extends App{
     Book(title = "Programming in Scala",
       authors = List("Odersky, Martin", "Spoon, Lex", "Venners, Bill"))
   )
-
   /**
     * find the names of all authors who have written at least two books presented in database "books"
     */
@@ -34,8 +34,61 @@ object MainWeek1 extends App{
       if a1 == a2
     } yield a1
   }.distinct
-
   println(query)
-}
 
-case class Book (title: String, authors: List[String])
+
+  /**
+    * lecture 2.2 - translation of for
+    */
+  def mapFun[T, U](xs: List[T], f: T => U): List[U] =
+    for (x <- xs) yield f(x)
+
+  def flatMapFun[T, U](xs: List[T], f: T => Iterable[U]): List[U] =
+    for (x <- xs; y <- f(x)) yield y
+
+  def filterFun[T](xs: List[T], p: T => Boolean): List[T] =
+    for (x <- xs; if p(x)) yield x
+
+  /**
+    * simple for expression
+    * for (x <- e1) yield e2
+    * will transform by compiler to
+    * e1.map(x => e2)
+    */
+
+  /**
+    * for expression
+    * for (x <- e1 if f; s) yield e2
+    * will transform by compiler to
+    * for (x <- e1.withFilter(x => f); s) yield e2
+    * and then translate to simplier expressions
+    */
+  /**
+    * s - mean is "something more sequence of generators or filters"
+    */
+  /**
+    * for expression
+    * for (x <- e1; y <- e2; s) yield e3
+    * will transform by compiler to
+    * e1.flatMap( x => for (y <- e2); s) yield e3)
+    * and then translate to simplier expressions
+    */
+  /**
+    * for expression
+    * for {
+    * i <- 1 until n
+    * j <- 1 until n
+    * if isPrime(i + j)
+    * } yield (i, j)
+    * will transform by compiler to
+    * (1 until n).flatMap( i =>
+    *   (1 until i).withFilter(j => isPrime(i+j))
+    *   .map(j => (i,j))
+    *   )
+    */
+  val forQuery = for(b <- books; a <- b.authors if a startsWith("Bird"))
+    yield b.title
+  val translatedQuery = books flatMap (b =>
+    b.authors withFilter(a => a startsWith "Bird") map(y => b.title))
+  println(translatedQuery)
+}
