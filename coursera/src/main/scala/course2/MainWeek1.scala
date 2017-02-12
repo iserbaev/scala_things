5a5d93bb75c06935dd245167f1ea849e6b305f02
ilnur <ilnur.serbaev@dz.ru>
2017-02-12 23:02:43 +0500

    week 1, lesson 1.3

--- coursera/src/main/scala/course2/MainWeek1.scala
+++ coursera/src/main/scala/course2/MainWeek1.scala
@@ -91,4 +91,93 @@ object MainWeek1 extends App{
   val translatedQuery = books flatMap (b =>
     b.authors withFilter(a => a startsWith "Bird") map(y => b.title))
   println(translatedQuery)
+
+  /**
+    * lecture 1.3 Functional random generators
+    */
+  trait Generator[+T] {
+    self =>  // an alias for "this"
+
+    def generate: T
+
+    def map[S](f: T => S): Generator[S] = new Generator[S] {
+      def generate = f(self.generate)
+    }
+    def flatMap[S](f: T => Generator[S]): Generator[S] = new Generator[S] {
+      def generate = f(self.generate).generate
+    }
+  }
+
+  val integers = new Generator[Int] {
+    val rand = new java.util.Random
+    def generate = rand.nextInt()
+  }
+  val booleansBoilerplate = new Generator[Boolean] {
+    def generate = integers.generate > 0
+  }
+  val booleans = integers map(x => x > 0)
+
+  val pairsBoilerplate = new Generator[(Int, Int)] {
+    def generate = (integers.generate, integers.generate)
+  }
+  def pairs[T, U](t: Generator[T], u: Generator[U]) =
+    t flatMap (x => u map(y => (x,y)))
+  def single[T](x: T): Generator[T] = new Generator[T] {
+    def generate = x
+  }
+  def choose(lo: Int, hi: Int): Generator[Int] = {
+    for (x <- integers) yield lo + x % (hi - lo)
+  }
+  def oneOf[T](xs: T*): Generator[T] =
+    for (idx <- choose(0, xs.length)) yield xs(idx)
+  def lists: Generator[List[Int]] = for {
+    isEmpty <- booleans
+    list <- if (isEmpty) emptyLists else nonEmptyLists
+  } yield list
+  def emptyLists = single(Nil)
+  def nonEmptyLists = for {
+    head <- integers
+    tail <- lists
+  } yield head :: tail
+
+  trait Tree
+  case class Inner(left: Tree, right: Tree) extends Tree
+  case class Leaf(x: Int) extends Tree
+  def leafs: Generator[Leaf] = for{
+    x <- integers
+  } yield Leaf(x)
+  def inners: Generator[Inner] = for {
+    l <- trees
+    r <- trees
+  } yield Inner(l, r)
+  def trees: Generator[Tree] = for {
+    isLeaf <- booleans
+    tree <- if (isLeaf) leafs else inners
+  } yield tree
+
+  /**
+    * tests generator
+    */
+  def test[T](g: Generator[T], numTimes: Int = 100)
+             (test: T => Boolean): Unit = {
+    for (i <- 0 until numTimes){
+      val  value = g.generate
+      assert(test(value), "test failed for " + value)
+    }
+    println("passed " + numTimes + " tests")
+  }
+  /**
+    * example of test using -> must be assertion failed
+    */
+  test(pairs(lists, lists)) {
+    case (xs: List[Int], ys: List[Int]) => (xs ++ ys).length > xs.length
+  }
+
+  /**
+    * ScalaCheck tool
+    *
+    * forAll { (l1: List[Int], l2: List[Int]) =>
+    *   l1.size + l2.size == (l1 ++ l2).size
+    * }
+  */
 }
