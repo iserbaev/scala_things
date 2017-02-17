cea35255d079c994eb3cb389321fe0927479ddf5
ilnur <ilnur.serbaev@dz.ru>
2017-02-17 13:19:10 +0500

    week 2 lecture 2.1

--- coursera/src/main/scala/course2/MainWeek2.scala
+++ coursera/src/main/scala/course2/MainWeek2.scala
@@ -10,10 +10,12 @@ object MainWeek2 extends App{
   abstract class IntSet{
     def incl(x: Int): IntSet
     def contains(x: Int): Boolean
+    def union(other: IntSet): IntSet
   }
   object Empty extends IntSet{
     def incl(x: Int): IntSet = NonEmpty(x, Empty, Empty)
     def contains(x: Int): Boolean = false
+    def union(other: IntSet): IntSet = other
   }
   case class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet{
     def incl(x: Int): IntSet = {
@@ -27,5 +29,17 @@ object MainWeek2 extends App{
       else if (x > elem) right contains x
       else true
     }
+
+    def union(other: IntSet): IntSet = (left union (right union (other))) incl elem
   }
+
+  /**
+    * Laws of IntSet
+    *
+    * 1. Empty contains x = false
+    * 2. (s incl x) contains x = true
+    * 3. (s incl x) contains y = s contains y (if x != y)
+    * 4. (xs union ys) contains x = xs contains x || ys contains x
+    */
+
 }
