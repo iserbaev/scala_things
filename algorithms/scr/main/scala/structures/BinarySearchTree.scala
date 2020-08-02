3a2ce13afa8b4222ff71600072fa4855c4edc4fc
serbaev_ilnur <serbaevilnur@gmail.com>
2020-08-02 22:21:25 +0500

    algs - binary search tree impl WIP
    predecessor

--- algorithms/scr/main/scala/structures/BinarySearchTree.scala
+++ algorithms/scr/main/scala/structures/BinarySearchTree.scala
@@ -6,25 +6,25 @@ import scala.annotation.tailrec
 import scala.collection.immutable
 
 sealed trait BinarySearchTree[+T] {
-  def key:    Option[T]
-  def left:   BinarySearchTree[T]
-  def right:  BinarySearchTree[T]
-  def parent: BinarySearchTree[T]
+  def key:         Option[T]
+  def left:        BinarySearchTree[T]
+  def right:       BinarySearchTree[T]
+  def parentLabel: Option[T]
 }
 
 case object Nil extends BinarySearchTree[Nothing] {
   override def key: Option[Nothing] = None
 
-  override def left:   BinarySearchTree[Nothing] = Nil
-  override def right:  BinarySearchTree[Nothing] = Nil
-  override def parent: BinarySearchTree[Nothing] = Nil
+  override def left:        BinarySearchTree[Nothing] = Nil
+  override def right:       BinarySearchTree[Nothing] = Nil
+  override def parentLabel: Option[Nothing]           = None
 }
 
 case class Node[+T](
-  k:      T,
-  left:   BinarySearchTree[T],
-  right:  BinarySearchTree[T],
-  parent: BinarySearchTree[T]
+  k:           T,
+  left:        BinarySearchTree[T],
+  right:       BinarySearchTree[T],
+  parentLabel: Option[T]
 ) extends BinarySearchTree[T] {
   override def key: Option[T] = k.some
 }
@@ -50,6 +50,7 @@ object BinarySearchTree {
     recur(List(x), List.empty)
   }
 
+  // O(log n)
   @tailrec
   def treeSearch[T](x: BinarySearchTree[T], t: T)(
     implicit ordering: Ordering[T]
@@ -94,55 +95,69 @@ object BinarySearchTree {
     recur(x, x)
   }
 
-  def treeSuccessor[T](x: BinarySearchTree[T])(
-    implicit ordering:    Ordering[Option[T]]
+  // O(log n)
+  def parent[T](root:  BinarySearchTree[T], parentLabel: Option[T])(
+    implicit ordering: Ordering[T]
+  ): BinarySearchTree[T] =
+    parentLabel.map(BinarySearchTree.treeSearch(root, _)).getOrElse(Nil)
+
+  def treeSuccessor[T](x: BinarySearchTree[T], root: BinarySearchTree[T])(
+    implicit opt:         Ordering[Option[T]],
+    tOrd:                 Ordering[T]
   ): BinarySearchTree[T] =
     x.right match {
       case value: Node[T] =>
         treeMin(value)
       case Nil =>
         @tailrec
-        def recur(xx: BinarySearchTree[T]): BinarySearchTree[T] =
-          xx.parent match {
-            case value: Node[T]
-                if value.right.key.isDefined && ordering
-                  .equiv(value.right.key, xx.key) =>
-              recur(value)
-            case Nil =>
-              xx
+        def recur(
+          y:  BinarySearchTree[T],
+          xx: BinarySearchTree[T]
+        ): BinarySearchTree[T] =
+          parent(root, y.parentLabel) match {
+            case yp: Node[T] if y.right.key == xx.key =>
+              recur(yp, y)
+            case _ =>
+              y
           }
-        recur(x.parent)
+        recur(parent(root, x.parentLabel), x)
     }
 
-  def treePredecessor[T](x: BinarySearchTree[T])(
-    implicit ordering:      Ordering[Option[T]]
+  def treePredecessor[T](x: BinarySearchTree[T], root: BinarySearchTree[T])(
+    implicit opt:           Ordering[Option[T]],
+    tOrd:                   Ordering[T]
   ): BinarySearchTree[T] =
     x.left match {
       case value: Node[T] =>
         treeMax(value)
       case Nil =>
         @tailrec
-        def recur(xx: BinarySearchTree[T]): BinarySearchTree[T] =
-          xx.parent match {
-            case value: Node[T]
-                if value.left.key.isDefined && ordering
-                  .equiv(value.left.key, xx.key) =>
-              recur(value)
-            case Nil =>
-              xx
+        def recur(
+          y:  BinarySearchTree[T],
+          xx: BinarySearchTree[T]
+        ): BinarySearchTree[T] =
+          parent(root, y.parentLabel) match {
+            case yp: Node[T] if y.left.key == xx.key =>
+              recur(yp, y)
+            case _ =>
+              y
           }
-        recur(x.parent)
+        recur(parent(root, x.parentLabel), x)
     }
 
-  def treeInsert[T](x: BinarySearchTree[T], v: T)(
+  def treeInsert[T](
+    x:           BinarySearchTree[T],
+    v:           T,
+    parentLabel: Option[T] = None
+  )(
     implicit ordering: Ordering[T]
   ): BinarySearchTree[T] =
     x match {
-      case Nil => Node(v, Nil, Nil, x)
+      case Nil => Node(v, Nil, Nil, parentLabel)
       case pp @ Node(k, l, _, _) if ordering.lt(v, k) =>
-        pp.copy(left = treeInsert(l, v))
+        pp.copy(left = treeInsert(l, v, pp.key))
       case pp @ Node(_, _, r, _) =>
-        pp.copy(right = treeInsert(r, v))
+        pp.copy(right = treeInsert(r, v, pp.key))
     }
 
   def treeInserts[T](x: BinarySearchTree[T], vv: T*)(
@@ -174,9 +189,20 @@ object TestTree extends App {
   println("treeMin = " + treeMin(result))
   println("treeMax = " + treeMax(result))
 
-  println("succ=" + treeSuccessor(result))
-  println("pred=" + treePredecessor(result))
+  println("treeSearch 15 = " + treeSearch(result, 15))
+  println("treeSearch 2 = " + treeSearch(result, 2))
+  println("treeSearch 17 = " + treeSearch(result, 17))
+  println("treeSearch 6 = " + treeSearch(result, 6))
+  println("treeSearch 20 = " + treeSearch(result, 20))
 
-  println(result.key)
+  println("parent 6 == 15 =" + parent(result, result.left.parentLabel))
+  println("parent 3 == 6 =" + parent(result, result.left.left.parentLabel))
+  println("parent 15 == Nil =" + parent(result, result.key))
+
+  println("succ 15 == 17 =" + treeSuccessor(result, result))
+  println("succ 13 == 15 =" + treeSuccessor(result.left.right.right, result))
+  println("pred 15 == 13 =" + treePredecessor(result, result))
+  println("pred 7 == 6 =" + treePredecessor(result.left.right, result))
+  println("pred 17 == 15 =" + treePredecessor(result.right.left, result))
 
 }
