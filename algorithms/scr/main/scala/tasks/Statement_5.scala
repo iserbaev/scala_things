836d20f860882a3ee21a9ec3471fded0c14edec7
i.serbaev <i.serbaev@kryptonite.ru>
2020-09-29 19:26:49 +0300

    statements = #5
    tests added

--- algorithms/scr/main/scala/tasks/Statement_5.scala
+++ algorithms/scr/main/scala/tasks/Statement_5.scala
@@ -38,7 +38,7 @@ object TestApp5 extends App {
     val aa = a.split(" ").map(_.toInt)
     val result =
       Statement_5.maxInWindow(aa.length, aa, m).mkString(" ")
-    assert(result == expected, s"($result) != ($expected)")
+    if (result != expected) println(s"($result) != ($expected)")
   }
 
   test("2 1 5", 1, "2 1 5")
@@ -49,4 +49,19 @@ object TestApp5 extends App {
     "73 97 97 97 97 97 97 97 42"
   )
   test("28 7 64 40 68 86 80 93 4 53 32 56 68 18 59", 12, "93 93 93 93")
+  test("16 79 20 19 43 72 78 33 40 52 70 79 66 43 60 ", 12, "79 79 79 79")
+
+  test(
+    "34 51 61 90 26 84 2 25 7 8 25 78 21 47 25",
+    3,
+    "61 90 90 90 84 84 25 25 25 78 78 78 47"
+  )
+
+  test(
+    "27 83 29 77 6 3 48 2 16 72 46 38 55 2 58",
+    5,
+    "83 83 77 77 48 72 72 72 72 72 58"
+  )
+
+  test("1 4 5 6 1 1 1 1", 4, "6 6 6 6 1")
 }
