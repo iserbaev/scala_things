c839be86aa9f43d0b1a56f43a639e244d3f8efb2
i.serbaev <i.serbaev@kryptonite.ru>
2023-08-17 11:23:58 +0300

    leetCode: Remove Duplicates from Sorted Array

---
+++ algorithms/src/main/scala/leetcode/RemoveDuplicatesFromSortedArray.scala
@@ -0,0 +1,16 @@
+package leetcode
+
+object RemoveDuplicatesFromSortedArray {
+  def removeDuplicates(nums: Array[Int]): Int = if (nums.nonEmpty) {
+    val result = nums.tail.foldLeft((1, 1, nums.head)) { case ((uniqueNums, indexToWrite, prevUniqueNum), num) =>
+      if (num != prevUniqueNum) {
+        nums.update(indexToWrite, num)
+        (uniqueNums + 1, indexToWrite + 1, num)
+      } else {
+        (uniqueNums, indexToWrite, prevUniqueNum)
+      }
+    }
+
+    result._1
+  } else 0
+}
