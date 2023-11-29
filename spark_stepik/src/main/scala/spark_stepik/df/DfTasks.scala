f7460bc8cc8fbdf715d9a4867d0446d99d64236d
i.serbaev <i.serbaev@kryptonite.ru>
2023-11-29 16:53:10 +0300

    spark_stepik: DfTask1 after review

--- spark_stepik/src/main/scala/spark_stepik/df/DfTasks.scala
+++ spark_stepik/src/main/scala/spark_stepik/df/DfTasks.scala
@@ -8,6 +8,32 @@ import spark_stepik.SparkCxt
 object DfTasks extends App with SparkCxt {
   override val appName: String = "DfTasks"
 
+  def withProperAge(df: DataFrame): DataFrame =
+    df.withColumn("Age", col("Age").plus(2))
+
+  def withGenderCode(df: DataFrame): DataFrame = {
+    val isFemale = col("Gender") === "Female"
+    val isMale = col("Gender") === "Male"
+
+    df.withColumn("gender_code",
+      when(isMale, 1)
+        .when(isFemale, 0)
+        .otherwise(-1))
+  }
+
+  def extractCustomerGroups(df: DataFrame): DataFrame = {
+    val columns = Seq(col("Gender"), col("Age"))
+    val hasProperAge = col("Age").between(30, 35)
+
+    df
+      .filter(hasProperAge)
+      .groupBy(columns: _*)
+      .agg(round(
+        avg("Annual Income (k$)"),
+        1).as("Avg_Income"))
+      .orderBy(columns: _*)
+  }
+
   val mallCustomersSchema: StructType = StructType(
     Seq(
       StructField("CustomerID", IntegerType),
@@ -25,19 +51,10 @@ object DfTasks extends App with SparkCxt {
     .option("path", "spark_stepik/src/main/resources/1_df_files/mall_customers.csv")
     .load()
 
-  val mallCustomersAgeFixedDF: DataFrame =
-    mallCustomersRawDF.withColumn("Age", col("Age") + 2)
-
-  val incomeDF: DataFrame = mallCustomersAgeFixedDF
-    .filter("30 <= Age")
-    .filter("Age <= 35")
-    .groupBy("Gender", "Age")
-    .agg(round(avg("Annual Income (k$)"), 1).as("Average Annual Income (k$)"))
-    .sort("Gender", "Age")
-
-  val isMale: Column = col("Gender") === "Male"
-  val incomeWithGenderCodeDF: DataFrame = incomeDF
-    .withColumn("gender_code", when(isMale, 1).otherwise(0))
+  val incomeWithGenderCodeDF = mallCustomersRawDF
+    .transform(withProperAge)
+    .transform(extractCustomerGroups)
+    .transform(withGenderCode)
 
   incomeWithGenderCodeDF.write
     .mode(SaveMode.Overwrite)
