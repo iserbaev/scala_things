929e7cd4bac344ddee8782349da71cb1db7bb630
i.serbaev <i.serbaev@kryptonite.ru>
2023-11-29 16:53:27 +0300

    spark_stepik: reformat

--- spark_stepik/src/main/scala/spark_stepik/df/DfTasks.scala
+++ spark_stepik/src/main/scala/spark_stepik/df/DfTasks.scala
@@ -13,24 +13,24 @@ object DfTasks extends App with SparkCxt {
 
   def withGenderCode(df: DataFrame): DataFrame = {
     val isFemale = col("Gender") === "Female"
-    val isMale = col("Gender") === "Male"
+    val isMale   = col("Gender") === "Male"
 
-    df.withColumn("gender_code",
+    df.withColumn(
+      "gender_code",
       when(isMale, 1)
         .when(isFemale, 0)
-        .otherwise(-1))
+        .otherwise(-1)
+    )
   }
 
   def extractCustomerGroups(df: DataFrame): DataFrame = {
-    val columns = Seq(col("Gender"), col("Age"))
+    val columns      = Seq(col("Gender"), col("Age"))
     val hasProperAge = col("Age").between(30, 35)
 
     df
       .filter(hasProperAge)
       .groupBy(columns: _*)
-      .agg(round(
-        avg("Annual Income (k$)"),
-        1).as("Avg_Income"))
+      .agg(round(avg("Annual Income (k$)"), 1).as("Avg_Income"))
       .orderBy(columns: _*)
   }
 
