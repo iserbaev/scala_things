51022cc90182193cc705ddf43046db52a379962c
i.serbaev <i.serbaev@kryptonite.ru>
2023-11-30 14:37:22 +0300

    spark_stepik: DFTask2 refactor

--- spark_stepik/src/main/scala/spark_stepik/df/DfTask2.scala
+++ spark_stepik/src/main/scala/spark_stepik/df/DfTask2.scala
@@ -1,13 +1,13 @@
 package spark_stepik.df
 
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{DataFrame, SaveMode}
 import org.apache.spark.sql.functions._
 import spark_stepik.SparkCxt
 
 object DfTask2 extends App with SparkCxt {
   override val appName: String = "DfTask2"
 
-  def withWords(wordAlias: String)(df: DataFrame): DataFrame =
+  def withWordsInLowerCase(wordAlias: String)(df: DataFrame): DataFrame =
     df
       .select(explode_outer(split(col("_c0"), "\\W+")).as(wordAlias))
       .filter(col(wordAlias) =!= "")
@@ -28,24 +28,25 @@ object DfTask2 extends App with SparkCxt {
       .option("inferSchema", "true")
       .csv("spark_stepik/src/main/resources/1_df_files/subtitles_s2.json")
 
-  subtitlesS1DF.printSchema()
-  subtitlesS2DF.printSchema()
-
   val s1WordCounts = subtitlesS1DF
-    .transform(withWords("w_s1"))
-    .transform(withWordFrequencies("w_s1","cnt_s1"))
+    .transform(withWordsInLowerCase("w_s1"))
+    .transform(withWordFrequencies("w_s1", "cnt_s1"))
 
   val s2WordCounts = subtitlesS2DF
-    .transform(withWords("w_s2"))
+    .transform(withWordsInLowerCase("w_s2"))
     .transform(withWordFrequencies("w_s2", "cnt_s2"))
 
   val joinCondition = s1WordCounts.col("w_s1") === s2WordCounts.col("w_s2")
 
-  val orderingColumns      = Seq(desc("cnt_s1"), desc("cnt_s2"))
+  val orderingColumns = Seq(desc("cnt_s1"), desc("cnt_s2"))
   val joinedSubtitlesS1S2 = s1WordCounts
     .join(s2WordCounts, joinCondition)
     .orderBy(orderingColumns: _*)
     .withColumn("id", monotonically_increasing_id())
+    .limit(20)
 
-  joinedSubtitlesS1S2.show(20)
+  joinedSubtitlesS1S2.write
+    .mode(SaveMode.Overwrite)
+    .save("spark_stepik/src/main/resources/data/wordcount")
+
 }
