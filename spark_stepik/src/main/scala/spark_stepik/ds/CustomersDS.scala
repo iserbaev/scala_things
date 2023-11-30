b86591e696e1dd3846a04ec96c58669de81b3c22
i.serbaev <i.serbaev@kryptonite.ru>
2023-11-30 14:54:55 +0300

    spark_stepik: CustomersDS with nulls

--- spark_stepik/src/main/scala/spark_stepik/ds/CustomersDS.scala
+++ spark_stepik/src/main/scala/spark_stepik/ds/CustomersDS.scala
@@ -25,4 +25,20 @@ object CustomersDS extends App with SparkCxt {
   val customersDS = customersDF.as[Customer]
 
   customersDS.show(10)
+
+  val customersWithNullsDF: DataFrame = spark.read
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .csv("spark_stepik/src/main/resources/2_ds_files/customers_with_nulls.csv")
+
+  case class CustomerWithNulls(
+      name: String,
+      age: Option[Int],
+      customer_rating: Option[Double])
+
+  val customersWithNullsDS = customersWithNullsDF.as[CustomerWithNulls]
+
+  customersWithNullsDS
+    .filter(customer => customer.customer_rating.getOrElse(0.0) > 4.0)
+    .show()
 }
