eaf05c2c4aba29e5ee0c4ba181fba28c64cb00f8
i.serbaev <i.serbaev@kryptonite.ru>
2023-11-30 18:00:51 +0300

    spark_stepik: rewrite DfTask2 after review

--- spark_stepik/src/main/scala/spark_stepik/df/DfTask2.scala
+++ spark_stepik/src/main/scala/spark_stepik/df/DfTask2.scala
@@ -1,51 +1,47 @@
 package spark_stepik.df
 
-import org.apache.spark.sql.{ DataFrame, SaveMode }
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{ DataFrame, SaveMode }
 import spark_stepik.SparkCxt
 
 object DfTask2 extends App with SparkCxt {
-  override val appName: String = "DfTask2"
 
-  def withWordsInLowerCase(wordAlias: String)(df: DataFrame): DataFrame =
+  def extractWordsInLowerCase(wordAlias: String)(df: DataFrame): DataFrame =
     df
-      .select(explode_outer(split(col("_c0"), "\\W+")).as(wordAlias))
+      .select(
+        explode_outer(split(lower(col("_c0")), "\\W+"))
+          .as(wordAlias)
+      )
       .filter(col(wordAlias) =!= "")
-      .select(lower(col(wordAlias)).as(wordAlias))
 
-  def withWordFrequencies(wordAlias: String, countAlias: String)(df: DataFrame): DataFrame =
+  def extractWordFrequencies(wordAlias: String, countAlias: String)(df: DataFrame): DataFrame =
     df
       .groupBy(wordAlias)
       .agg(count(col(wordAlias)).as(countAlias))
+      .orderBy(desc(countAlias))
+      .withColumn("id", monotonically_increasing_id())
+      .limit(20)
+
+  def getJoinedStats(joinDf: DataFrame)(df: DataFrame): DataFrame =
+    joinDf
+      .join(df, joinDf.col("id") === df.col("id"))
+      .drop(df.col("id"))
+
+  def read(file: String, wordAlias: String, countAlias: String) =
+    spark.read
+      .option("inferSchema", "true")
+      .csv(file)
+      .transform(extractWordsInLowerCase(wordAlias))
+      .transform(extractWordFrequencies(wordAlias, countAlias))
 
   val subtitlesS1DF =
-    spark.read
-      .option("inferSchema", "true")
-      .csv("spark_stepik/src/main/resources/1_df_files/subtitles_s1.json")
-
+    read("spark_stepik/src/main/resources/1_df_files/subtitles_s1.json", "w_s1", "cnt_s1")
   val subtitlesS2DF =
-    spark.read
-      .option("inferSchema", "true")
-      .csv("spark_stepik/src/main/resources/1_df_files/subtitles_s2.json")
+    read("spark_stepik/src/main/resources/1_df_files/subtitles_s2.json", "w_s2", "cnt_s2")
 
-  val s1WordCounts = subtitlesS1DF
-    .transform(withWordsInLowerCase("w_s1"))
-    .transform(withWordFrequencies("w_s1", "cnt_s1"))
+  val joinedSubtitlesS1S2DF = getJoinedStats(subtitlesS1DF)(subtitlesS2DF)
 
-  val s2WordCounts = subtitlesS2DF
-    .transform(withWordsInLowerCase("w_s2"))
-    .transform(withWordFrequencies("w_s2", "cnt_s2"))
-
-  val joinCondition = s1WordCounts.col("w_s1") === s2WordCounts.col("w_s2")
-
-  val orderingColumns = Seq(desc("cnt_s1"), desc("cnt_s2"))
-  val joinedSubtitlesS1S2 = s1WordCounts
-    .join(s2WordCounts, joinCondition)
-    .orderBy(orderingColumns: _*)
-    .withColumn("id", monotonically_increasing_id())
-    .limit(20)
-
-  joinedSubtitlesS1S2.write
+  joinedSubtitlesS1S2DF.write
     .mode(SaveMode.Overwrite)
     .save("spark_stepik/src/main/resources/data/wordcount")
 
