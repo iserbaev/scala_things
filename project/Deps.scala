5256dc62654355eff15b64285116f0cf50daeed1
ilnur.serbaev <serbaevilnur@gmail.com>
2024-05-16 19:51:28 +0500

    chore: add newtype, tapir, derevo, tofu

--- project/Deps.scala
+++ project/Deps.scala
@@ -1,40 +1,54 @@
-import sbt._
+import sbt.*
 
 object Deps {
   object Versions {
-    val Scala       = "2.13.14"
-    val cats        = "2.10.0"
-    val catsEffect  = "3.5.4"
-    val catsTagless = "0.16.0"
-    val shapeless   = "2.3.10"
+    val Scala = "2.13.14"
 
     // Compiler Plugins
-    val BetterMonadicFor = "0.3.1"
-    val KindProjector = "0.13.3"
-    val SemanticDB = "4.8.12"
+    val BetterMonadicFor  = "0.3.1"
+    val KindProjector     = "0.13.3"
+    val SemanticDB        = "4.8.12"
     val ScalafixTypelevel = "0.2.0"
+
+    val breeze = "2.1.0"
+    val spire  = "0.18.0"
+
+    val cats        = "2.10.0"
+    val catsEffect  = "3.5.4"
+    val catsMtl     = "1.2.1"
+    val catsTagless = "0.16.0"
+
+    val derevo = "0.12.5"
+
+    val doobie = "1.0.0-RC5"
+
+    val fs2       = "3.10.2"
+    val fs2_kafka = "3.0.1"
+
+    val quickLens = "1.9.3"
+
+    val monocle = "3.0.0"
+
+    val newtype = "0.4.4"
+
+    val refined = "0.9.25"
+
+    val circe         = "0.14.7"
+    val http4sVersion = "0.23.27"
+
+    val shapeless = "2.3.10"
+    val spark     = "3.5.0"
+
+    val SttpApispec = "0.8.0"
+    val SttpClient3 = "3.9.5"
+    val SttpModel   = "1.7.10"
+    val SttpShared  = "1.3.18"
+    val SttpTapir   = "1.10.7"
+
+    val tofu = "0.13.0"
 
     val scalaLogging = "3.9.2"
     val logback      = "1.4.11"
-
-    val fs2 = "3.10.0"
-
-    val circe         = "0.14.6"
-    val http4sVersion = "0.23.18"
-
-    val doobieVersion = "1.0.0-RC5"
-
-    val fs2_kafka = "3.0.1"
-
-    val spark ="3.5.0"
-    val SttpApispec        = "0.8.0"
-    val SttpClient3        = "3.9.5"
-    val SttpModel          = "1.7.9"
-    val SttpShared         = "1.3.17"
-    val SttpTapir          = "1.10.0"
-
-    val spire  = "0.18.0"
-    val breeze = "2.1.0"
 
     val scalaTest             = "3.0.8"
     val testcontainers        = "0.41.3"
@@ -42,7 +56,6 @@ object Deps {
     val postgresTestcontainer = "1.12.3"
     val Weaver                = "0.8.4"
 
-    val quickLens = "1.9.3"
   }
 
   private lazy val lens = Seq(
@@ -68,6 +81,10 @@ object Deps {
     "org.typelevel" %% "cats-free" % Versions.cats
   )
 
+  private lazy val catsMtl = Seq(
+    "org.typelevel" %% "cats-mtl" % Versions.catsMtl
+  )
+
   private lazy val catsEffect = Seq(
     "org.typelevel" %% "cats-effect" % Versions.catsEffect
   )
@@ -87,8 +104,10 @@ object Deps {
     "com.typesafe" % "config" % "1.3.4"
   )
 
-  private lazy val shapeless = Seq(
-    "com.chuusai" %% "shapeless" % Versions.shapeless
+  val derevo = Seq(
+     "tf.tofu" %% "derevo-cats"% Versions.derevo,
+     "tf.tofu" %% "derevo-cats-tagless"% Versions.derevo,
+     "tf.tofu" %% "derevo-circe-magnolia" % Versions.derevo,
   )
 
   private lazy val http4s = Seq(
@@ -98,11 +117,11 @@ object Deps {
   )
 
   private lazy val doobie = Seq(
-    "org.tpolecat" %% "doobie-core"     % Versions.doobieVersion,
-    "org.tpolecat" %% "doobie-h2"       % Versions.doobieVersion,
-    "org.tpolecat" %% "doobie-hikari"   % Versions.doobieVersion, // HikariCP transactor.
-    "org.tpolecat" %% "doobie-postgres" % Versions.doobieVersion, // Postgres driver 42.2.8 + type mappings
-    "org.tpolecat" %% "doobie-scalatest" % Versions.doobieVersion % "test" // ScalaTest support for typechecking statements.
+    "org.tpolecat" %% "doobie-core"      % Versions.doobie,
+    "org.tpolecat" %% "doobie-h2"        % Versions.doobie,
+    "org.tpolecat" %% "doobie-hikari"    % Versions.doobie, // HikariCP transactor.
+    "org.tpolecat" %% "doobie-postgres"  % Versions.doobie, // Postgres driver 42.2.8 + type mappings
+    "org.tpolecat" %% "doobie-scalatest" % Versions.doobie % "test" // ScalaTest support for typechecking statements.
   )
 
   private lazy val fs2 = Seq(
@@ -114,8 +133,30 @@ object Deps {
     "com.github.fd4s" %% "fs2-kafka" % Versions.fs2_kafka
   )
 
+  val monocle = Seq(
+    "dev.optics" %% "monocle-core"  % Versions.monocle,
+    "dev.optics" %% "monocle-macro" % Versions.monocle,
+  )
+
+  val newtype = Seq(
+    "io.estatico" %% "newtype" % Versions.newtype
+  )
+
+  val refined = Seq(
+    "eu.timepit" %% "refined"      % Versions.refined,
+    "eu.timepit" %% "refined-cats" % Versions.refined,
+  )
+
+  private lazy val shapeless = Seq(
+    "com.chuusai" %% "shapeless" % Versions.shapeless
+  )
+
   private lazy val spire = Seq(
     "org.typelevel" %% "spire" % Versions.spire
+  )
+
+  val tofu = Seq(
+    "tf.tofu" %% "tofu-core-higher-kind" % Versions.tofu
   )
 
   val weaver = Seq(
@@ -141,8 +182,8 @@ object Deps {
   )
 
   val spark = Seq(
-    "org.apache.spark" %% "spark-core" % Versions.spark,
-    "org.apache.spark" %% "spark-sql" % Versions.spark,
+    "org.apache.spark" %% "spark-core"      % Versions.spark,
+    "org.apache.spark" %% "spark-sql"       % Versions.spark,
     "org.apache.spark" %% "spark-streaming" % Versions.spark,
   )
 
